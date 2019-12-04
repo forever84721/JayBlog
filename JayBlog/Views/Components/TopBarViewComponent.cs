6f8b217c1941ca099897ab2a10b2404c95b7ea75
Jay <forever84721@yahoo.com.tw>
2019-12-04 22:56:49 +0800

    init

---
+++ JayBlog/Views/Components/TopBarViewComponent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JayBlog.Views.Components
+{
+    [Microsoft.AspNetCore.Mvc.ViewComponent]
+    public class TopBarViewComponent : Microsoft.AspNetCore.Mvc.ViewComponent
+    {
+        public string Invoke()
+        {
+            return $"Test";
+        }
+    }
+}
