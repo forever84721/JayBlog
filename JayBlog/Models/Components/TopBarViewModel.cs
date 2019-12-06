82c1eefc39f7f83737b6aab126e40ba8cee1ded9
Jay <forever84721@yahoo.com.tw>
2019-12-07 01:00:22 +0800

    TopBar

---
+++ JayBlog/Models/Components/TopBarViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JayBlog.Models.Components
+{
+    public class TopBarViewModel
+    {
+        public int A { get; set; }
+        public string B { get; set; }
+    }
+}
---
+++ JayBlog/Views/Shared/Components/TopBar/TopBarViewComponent.cs
@@ -0,0 +1,18 @@
+﻿using JayBlog.Models.Components;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JayBlog.Views.Shared.Components.TopBar
+{
+    [ViewComponent]
+    public class TopBarViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()//async Task
+        {
+            return View("TopBar", new TopBarViewModel() { A = 1, B = "88" });
+        }
+    }
+}
